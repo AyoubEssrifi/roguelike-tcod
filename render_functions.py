488c7ca6bda96aa8bd1737b089d8b04c2bfb3724
AyoubEssrifi <essrifiayoub@gmail.com>
2021-03-26 17:12:14 +0100

    delete comments

--- render_functions.py
+++ render_functions.py
@@ -60,7 +60,6 @@ def render_all(con, panel, entities, player, screen_width, screen_height, game_m
                     game_map.tiles[x][y].explored = True
                     
                     if wall:
-                        # libtcod.console_set_char_background(con, x, y, colors.get('light_wall'), libtcod.BKGND_SET)
                         libtcod.console_put_char_ex(con, x, y, tiles['wall_tile'], libtcod.white, libtcod.black)
                     else:
                         libtcod.console_put_char_ex(con, x, y, tiles['floor_tile'], libtcod.white, libtcod.black)
@@ -114,7 +113,6 @@ def clear_all(con, entities):
     
 def draw_entity(con, fov_map, entity):
     if libtcod.map_is_in_fov(fov_map, entity.x, entity.y):
-        # libtcod.console_set_default_foreground(con, entity.color)
         libtcod.console_put_char_ex(con, entity.x, entity.y, entity.char, entity.color, libtcod.black)
         
 def clear_entity(con, entity):
